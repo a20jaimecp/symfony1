95a8d950e596e14fc2bba60d42edff519eba49a9
JaimeCP <a20jaimecp@iessanclemente.net>
2023-03-19 22:09:01 +0100

    first one

---
+++ app/entrypoint-dev.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+/usr/sbin/php-fpm8.1  
+
+symfony serve --dir /app/symfony/my_project
---
+++ app/entrypoint-nginx.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+/usr/sbin/php-fpm8.1 -F 
+
+#symfony serve --dir /app/symfony/my_project
